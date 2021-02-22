f2b6d0302261a512809874932828ab3c09c41baf
stefi23 <stefania.rosca2309@gmail.com>
2021-02-22 12:58:19 +0100

    added API, helper functions and QuestionCard Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { fetchQuizQuestions, Difficulty } from './API'
 import QuestionCard from './components/QuestionCard'
 
+
+const TOTAL_QUESTIONS = 10
+
 function App() {
+  const [loading, setLoading] = useState(false)
+  const [questions, setQuestions] = useState([])
+  const [number, setNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState([])
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
+ 
+ console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY ))
+
   const startQuiz = async () => {
 
   }
@@ -20,7 +33,14 @@ function App() {
       <button className='start' onClick={startQuiz}>Start</button>
       <p className='score'>Score:</p>
       <p>Loading Questions ...</p>
-      <QuestionCard />
+      {/* <QuestionCard 
+        questionNr={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+        /> */}
       <button className='next' onClick={nextQuestion}>Next Question</button>
     </div>
 
